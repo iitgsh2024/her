53e65a4b404f9b25fe384fbaca388e1bef1c312b
iitgsh2024 <sshelss192837@gmail.com>
2025-02-10 02:31:16 +0530

    kal video embed karenge

--- app/page.js
+++ app/page.js
@@ -3,22 +3,24 @@
 import React, { useState, useEffect } from 'react';
 import Envelope from "../components/Envelope.jsx";
 import "./heart.css"
+import { Heading1 } from 'lucide-react';
 
-let amount = 40;
+let amount = 60;
 
 export default function Home() {
 
   const [isOpen, setIsOpen] = useState(false);
   const [isYes, setIsYes] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
+  const [showLast, setShowLast] = useState(0);
 
   const openEnvelope = () => {
       setIsOpen(true);
   };
 
-  const showHearts = ()=>{
-    return (
-      <div className="for-love-hearts" suppressHydrationWarning={true}>
+  const Hearts = 
+    (
+      <div className="for-love-hearts">
         {
           Array(amount).fill("heart").map((item,index) => {
             const randomNumberLeft = Math.floor(Math.random() * 101);
@@ -27,23 +29,38 @@ export default function Home() {
           })
         }
       </div>
-    );
-  }
+  );
+
+  const lastMessage = (
+    <h1 className="bg-red-200">I always knew you still loved me 😏</h1>
+  );
 
   useEffect(()=>{
     setIsMounted(true);
   },[])
 
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      setShowLast(prev=>prev+1);
+      clearInterval(interval);
+    }, 2000);
+  },[isYes&isOpen])
+
+  useEffect(()=>{
+    console.log(showLast);
+  },[showLast]);
+
   if(!isMounted){
       return null;
   }
-  
   return (
     <>
     <div onClick={openEnvelope}>
-      <Envelope isOpen={isOpen} setIsYes={setIsYes}> </Envelope>
+      <Envelope isOpen={isOpen} setIsYes={setIsYes} isYes={isYes}> </Envelope>
     </div>
-    {(isYes && isOpen) ? showHearts() : <></>}
+    {(isYes && isOpen) ? Hearts : <></>}
+    {(showLast>1) ? lastMessage : <></>}
+
     </>
   );
 }
--- components/Envelope.jsx
+++ components/Envelope.jsx
@@ -1,6 +1,12 @@
+import {Tangerine} from 'next/font/google';
 import { useState, useEffect } from 'react';
 import "./Envelope.css";
 import { Button } from './ui/button.jsx';
+
+const tangerine = Tangerine({
+    subsets:['latin'],
+    weight : "700",
+});
 
 const EnvelopeReveal = (props) => {
 
@@ -16,25 +22,25 @@ const EnvelopeReveal = (props) => {
     }, []);
 
     const yay = ()=>{
-        props.setIsYes(true);
+        if(props.isOpen) props.setIsYes(true);
     }
 
     const generateYesButtons = ()=>{
+        if(!props.isOpen) return;
         const randomTop = Math.floor(Math.random() * 101);
         const randomLeft = Math.floor(Math.random() * 101);
         let temp = yesButtonsList;
         temp.push(
         <div key={yesButtonsList.length+1} style={{position: "absolute", top: `${randomTop}%`, left: `${randomLeft}%` }} onClick={yay}>
-            <Button className="bg-red-400 hover:bg-pink-300">Yes</Button>
+            <Button className="bg-red-400 active:bg-pink-300 text-lg hover:bg-red-400">Yes</Button>
         </div>
         );
         setYesButtonsList(temp);
-        console.log(yesButtonsList);
     }
 
     return (
         <div className={"wrapper " +  (animate && !props.isOpen ? "wobble-hor-bottom" : "")}>
-            <div className={`container ${props.isOpen ? "open" : ""}`}>
+            <div className={`container ${props.isOpen ? "open" : ""} ${props.isYes ? "accept" : ""}`}>
                 <div className="envelope">
                     <div className="flap"></div>
                 </div>
@@ -42,10 +48,10 @@ const EnvelopeReveal = (props) => {
                     <p>Will you be my valentine?</p>
                     <div className="options">
                         <div className="yes" onClick={yay}>
-                            <Button className="bg-red-400 hover:bg-pink-300">Yes</Button>
+                            <Button className="bg-red-400 active:bg-pink-300 text-lg hover:bg-red-400">Yes</Button>
                         </div>
                         <div className="no" onClick={generateYesButtons}>
-                            <Button className="bg-red-400 hover:bg-pink-300">No</Button>
+                            <Button className="bg-red-400 active:bg-pink-300 text-lg hover:bg-red-400">No</Button>
                         </div>
                     </div>
                     {yesButtonsList.map((button)=>{
